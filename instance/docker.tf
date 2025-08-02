ce22cd6bcc32003a5440a353cc44d4a4de9718cd
Nagaraj <nagarajuande255@gmail.com>
2025-08-02 07:35:36 +0530

    K8

--- instance/docker.tf
+++ instance/docker.tf
@@ -1,11 +1,11 @@
 # This script will run instance & security Group inbound rules & ourbound rules 
 resource "aws_instance" "K8" {
   ami           = "ami-09c813fb71547fc4f"
-  instance_type = "t3.medium" # Instance type, can be changed as needed
+  instance_type = "t3.micro" # Instance type, can be changed as needed
   vpc_security_group_ids = [aws_security_group.allow_all_K8.id] # Security Group ID
 
   root_block_device {
-    volume_size = 50
+    volume_size = 50 # Size of the root volume in GB
     volume_type = "gp3" # or "gp2", depending on your preference
   }
 
